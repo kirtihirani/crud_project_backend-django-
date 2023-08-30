1bba69f920179d2d5210dc1f5b1a95b546ee74c1
kirtihirani <kirtihirani89@gmail.com>
2023-08-31 00:55:36 +0530

    put-delete-methods

--- crud_app_backend_django/EmployeeApp/urls.py
+++ crud_app_backend_django/EmployeeApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from EmployeeApp import views
 
 urlpatterns = [
-    re_path(r'^departments$', views.departmentApi)
+    re_path(r'^departments$', views.departmentApi),
+    re_path(r'^departments/([0-9]+)$',views.departmentApi)
 ]
--- crud_app_backend_django/EmployeeApp/views.py
+++ crud_app_backend_django/EmployeeApp/views.py
@@ -10,9 +10,14 @@ from EmployeeApp.serializers import DepartmentSerializer,EmployeeSerializer
 @csrf_exempt
 def departmentApi(request, id=0):
     if request.method=='GET':
-        departments = Departments.objects.all()
-        dept_serializer = DepartmentSerializer(departments,many=True)
-        return JsonResponse(dept_serializer.data,safe=False)
+        if id==0:
+            departments = Departments.objects.all()
+            dept_serializer = DepartmentSerializer(departments,many=True)
+            return JsonResponse(dept_serializer.data,safe=False)
+        else:
+            dept = Departments.objects.get(DepartmentId=id)
+            dept_srl = DepartmentSerializer(dept)
+            return JsonResponse(dept_srl.data,safe=False)
     elif request.method=='POST':
         dept_data = JSONParser().parse(request)
         dept_serial = DepartmentSerializer(data=dept_data)
@@ -21,7 +26,18 @@ def departmentApi(request, id=0):
             return JsonResponse("Added succefully",safe=False)
         else:
             return JsonResponse("adding data failed",safe=False)
-
+    elif request.method=='DELETE':
+        dept = Departments.objects.get(DepartmentId=id)
+        dept.delete()
+        return JsonResponse("deleted successfully",safe=False)
+    elif request.method=='PUT':
+         dept = Departments.objects.get(DepartmentId=id)
+         deptData = JSONParser().parse(request) 
+         dsrl = DepartmentSerializer(dept,data = deptData)
+         if dsrl.is_valid():
+            dsrl.save()
+            return JsonResponse("updated successfully",safe=False) 
+         return JsonResponse("failed to update",safe=False) 
      
 
     
