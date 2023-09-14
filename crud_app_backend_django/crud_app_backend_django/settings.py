3c34552ecba44b98568aa8b8fc41270766353d42
kirtihirani <kirtihirani89@gmail.com>
2023-09-14 14:01:08 +0530

    corsheader_line_added

--- crud_app_backend_django/crud_app_backend_django/settings.py
+++ crud_app_backend_django/crud_app_backend_django/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
